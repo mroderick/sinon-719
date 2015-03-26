12e3b32449e8c79673d5b9b3a4566000f4ba2254
Morgan Roderick <morgan@roderick.dk>
2015-03-26 09:40:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sinon.JS Issue 719
+
+This repository is for replicating the bug reported as [#719](https://github.com/cjohansen/Sinon.JS/issues/719) for Sinon.JS.
+
+```
+$ nvm use 0.10.28
+$ npm install
+$ node issue.js
+```
+
---
+++ issue.js
@@ -0,0 +1,9 @@
+// var sinon = require('sinon');
+var jsdom = require('jsdom');
+
+
+// global.document = jsdom('<html><body></body></html>');
+// global.window = document.parentWindow;
+
+// var sinonSandbox = sinon.sandbox.create();
+// sinonSandbox.stub(window, 'confirm', function() { return true; });

5700c39e3d3646fdd1473e0f24ba7f0814cef09f
Isaac Hodes <isaachodes@gmail.com>
2015-03-26 17:49:19 -0400

    pin jsdom to repro issue

--- issue.js
+++ issue.js
@@ -4,7 +4,7 @@ var jsdom = require('jsdom').jsdom;
 global.document = jsdom('<html><body></body></html>');
 console.log('global.document.parentWindow', global.document.parentWindow);
 
-// global.window = document.parentWindow;
+global.window = document.parentWindow;
 
-// var sinonSandbox = sinon.sandbox.create();
-// sinonSandbox.stub(global.window, 'confirm', function() { return true; });
+var sinonSandbox = sinon.sandbox.create();
+sinonSandbox.stub(global.window, 'confirm', function() { return true; });
